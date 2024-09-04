7f9b1a36a480f4939301a790cc294810a18c2800
Ks-Nath <86104360+Ks-Nath@users.noreply.github.com>
2024-09-04 18:04:12 +0530

    Update Learn101.py

--- Learn101.py
+++ Learn101.py
@@ -1,11 +1,16 @@
-import streamlit as st, pandas as pf, yfinance as yf
+import streamlit as st, pandas as pf, yfinance as yf, numpy as np
 import plotly.express as px 
 
 # Page title
-st.set_page_config(page_title='Stock Dashboard', page_icon='📊')
 st.title('📊 Stock Dashboard')
 ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
 
+data = yf.download(ticker, start=start_date, end=end_date)
+fig = px.line(data, x = data.index, y = data['Adj Close'], title=ticker)
+st.plotly_chart(fig)
 
+result = st.button("Show data")
+if result:
+    st.write(data)
