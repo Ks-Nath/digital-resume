b6530cb6afeeffe16cd62e1fec6c75e8b595d452
Ks-Nath <86104360+Ks-Nath@users.noreply.github.com>
2024-09-04 17:36:53 +0530

    Update Learn101.py

--- Learn101.py
+++ Learn101.py
@@ -1,5 +1,6 @@
 import streamlit as st, pandas as pf, yfinance as yf
- 
+import plotly.express as px
+
 # Page title
 st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
 st.title('📊 Intel')
