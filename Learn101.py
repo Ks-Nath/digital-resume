3cb30afd7c0d691067bab7d94f495bda18c24eb3
ks srinath <kssrinath@kss-MacBook-Pro.local>
2024-09-04 15:07:59 +0530

    initial commit

---
+++ Learn101.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title="My webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I am Nath")
+st.title("Just having fun")
+st.write("[Learn More >](https://ksnath.com)")
+
+
